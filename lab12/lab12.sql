b18feb7d4106f98ec36a723fdfa5a2a7e8fc7498
airbust <airbust@qq.com>
2018-08-29 12:06:48 +0800

    lab12 finished

--- lab12/lab12.sql
+++ lab12/lab12.sql
@@ -3,12 +3,14 @@
 
 -- Q2
 CREATE TABLE obedience AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT seven, denero FROM students;
 
 -- Q3
 CREATE TABLE smallest_int AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT time, smallest FROM students WHERE smallest > 15 ORDER BY smallest LIMIT 20;
 
 -- Q4
 CREATE TABLE matchmaker AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT a.pet, a.song, a.color, b.color
+    FROM students AS a, students AS b
+    WHERE a.pet = b.pet AND a.song = b.song AND a.time < b.time;
--- lab12/lab12_extra.sql
+++ lab12/lab12_extra.sql
@@ -2,36 +2,41 @@
 
 -- Q5
 CREATE TABLE greatstudents AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT a.date, a.color, a.pet, a.number, b.number
+    FROM students AS a, fa17students AS b
+    WHERE a.date = b.date AND a.color = b.color AND a.pet = b.pet;
 
 -- Q6
 CREATE TABLE sevens AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT a.seven
+    FROM students AS a, checkboxes AS b
+    WHERE a.number = 7 and b."7" = "True" and a.time = b.time;
 
 -- Q7
 CREATE TABLE fa17favnum AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT number, COUNT(*) FROM fa17students GROUP BY number ORDER BY -COUNT(*) LIMIT 1;
 
 
 CREATE TABLE fa17favpets AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT pet, COUNT(*) FROM fa17students GROUP BY pet ORDER BY -COUNT(*) LIMIT 10;
 
 
 CREATE TABLE sp18favpets AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT pet, COUNT(*) FROM students GROUP BY pet ORDER BY -COUNT(*) LIMIT 10;
 
 
 CREATE TABLE sp18dog AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT pet, COUNT(*) FROM students WHERE pet = "dog" GROUP BY pet;
 
 
 CREATE TABLE sp18alldogs AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT "dog", COUNT(*) FROM students WHERE pet LIKE "%dog%";
 
 
 CREATE TABLE obedienceimages AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT seven, denero, COUNT(*) FROM students WHERE seven = "7" GROUP BY denero;
+
 
 -- Q8
 CREATE TABLE smallest_int_count AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT smallest, COUNT(*) FROM students GROUP BY smallest;
